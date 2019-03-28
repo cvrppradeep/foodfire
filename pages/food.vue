818ccef21700dde433926ea7ed57fa342f27aa8e
itswadesh <itswadesh@gmail.com>
2019-03-28 23:41:03 +0530

    food page

---
+++ pages/food.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div
+      v-for="(f,ix) in foods"
+      :key="ix"
+    >
+      <label>{{f}}</label>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const foods = await $axios.$get("foods/group");
+    return { foods };
+  }
+};
+</script>
+
+<style scope>
+</style>
