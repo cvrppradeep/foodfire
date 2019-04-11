a9080e473fa559442798c9a67272b7a057e6b48e
cvrppradeep <43846840+cvrppradeep@users.noreply.github.com>
2019-04-11 13:21:42 +0530

    chefs page modifed

--- pages/food/chefs.vue
+++ pages/food/chefs.vue
@@ -2,14 +2,16 @@
   <div>
     <Header />
     <div class="heading">Registered Chefs</div>
-    <div class="card">
-      <ul>
-        <li
+    <div>
+      <ul class="p-left">
+        <li class="card"
           v-for="c in chefs"
           :key="c._id"
         >
+        <div class="font">
           <h1>{{c.restaurant}}</h1>
           <Ratingcircle rating="3.5" /><br /><br />
+        </div>
           <v-chip
             v-if="c.dish1"
             class="red"
@@ -56,7 +58,21 @@ ul > li {
   list-style: none;
   margin: 10px;
   padding: 10px;
-  border-bottom: 1px solid grey;
+  /* border-bottom: 1px solid grey; */
+}
+.card{
+padding: 1rem;
+    margin: 1rem;
+    -webkit-box-shadow: 0 -0.1rem 1.1rem rgba(0, 0, 0, 0.175) !important;
+    box-shadow: 0 -0.1rem 1.1rem rgba(0, 0, 0, 0.175) !important;
+    border-radius: 0.5rem;
+}
+.p-left{
+padding-left: 0px;
+}
+.font{
+ padding-left: 1rem;
+
 }
 </style>
 
